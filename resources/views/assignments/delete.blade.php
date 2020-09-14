f4fa197f7ea1f256f1695c5a0188f3c074710f8f
Paweł Kapela <mysio175@gmail.com>
2020-09-14 10:05:15 +0200

    Update delete.blade.php

--- resources/views/assignments/delete.blade.php
+++ resources/views/assignments/delete.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+@section('styles')
+	<link href="{{ asset('css/mathquill4quill.min.css') }}" rel="stylesheet">
+@endsection
+  
 @section('content')
-
+<div class="container">
+	
+</div>
 @endsection
